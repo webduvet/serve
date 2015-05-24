2146d5bbcfc69c9c1dfc46b2ef7a9483e665ed5e
webduvet <andrej.bartko@gmail.com>
2015-05-24 11:55:33 +0100

    making it working for the first time

--- bin/start.js
+++ bin/start.js
@@ -1,8 +1,22 @@
-console.log('start', process.argv);
-var
-	args = process.argv.length > 2 ? process.argv.slice(-2) : [,],
-	port = /^\d{4}$/.test(args[0]) ? args[0] : 3001,
-	path = (typeof args[1]!== 'undefined' && /^(((\/)|(\.)\/|(\.\.)\/))*(((\w+)|(\.\.)|(\.))*\/)*(\w*)$/.test(args[1])) ? args[1] : './';
+
+var Home = require('../lib/serve.js');
+
+var 
+	argv = process.argv,
+	portIndex = argv.indexOf('--port'),
+	pathIndex = argv.indexOf('--home'),
+	port = 3001,
+	path = './'
+	;
+
+if (portIndex >=0 ) {
+	port = /^\d{4}$/.test(argv[portIndex + 1]) ? argv[portIndex + 1] : port;
+}
+
+if (pathIndex >= 0) {
+	path = /^(((\/)|(\.)\/|(\.\.)\/))*(((\w+)|(\.\.)|(\.))*\/)*(\w*)$/.test(argv[pathIndex + 1]) ? argv[pathIndex + 1] : path;
+}
+
 
 // treatment for path in current dir with prefix ./ and trailing / if missing
 if (/^\w+/.test(path) ) {
@@ -12,6 +26,6 @@ if (/\w+$/.test(path) ) {
 	path = path + '/';
 }
 
-console.log(args);
-console.log(port, path);
+var home = new Home({port, path});
 
+home.run();
